9e8c061117d23e4aa02a9e51f3d297cf841918d5
Daniel Lopez Iglesias <dlopez-i@car3s3.42barcelona.com>
2022-07-15 16:37:41 +0200

    EX01 y EX02. Punteros

--- PROYECTOS/shell01/ex07/r_dwssap.sh
+++ PROYECTOS/shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | sed 's/:\(.*\)//g' | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | tr '\n' ' ' | sed 's/ /, /g' | sed '
+cat /etc/passwd | sed '/^#/d' | sed -n 'n;p' | sed 's/:\(.*\)//g' | rev | sort -r | sed -n "$FT_LINE1, $FT_LINE2 p" | tr '\n' ' ' | sed 's/ /, /g' | sed 's/\(.*\), /\1./' | tr -d '\n'
